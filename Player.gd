274b3b49d132cbb76627cf0e38d1b45a8cab6361
Jakub Tomšů <tomsuon@student.uzlabina.cz>
2022-10-14 13:01:47 +0200

    player

--- Player.gd
+++ Player.gd
@@ -5,6 +5,7 @@ extends RigidBody2D
 # var a = 2
 # var b = "text"
 
+var charge_time: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +14,7 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var dir: Vector2
+	var dir = Vector2.ZERO
 	if Input.is_action_pressed("player_left"):
 		dir += Vector2.LEFT
 	if Input.is_action_pressed("player_right"):
@@ -23,5 +24,17 @@ func _process(delta):
 	if Input.is_action_pressed("player_down"):
 		dir += Vector2.DOWN
 		
-	linear_velocity += dir * 10 * delta
+	if Input.is_action_pressed("player_charge"):
+		charge_time += delta
+
+	if Input.is_action_just_released("player_charge"):
+		shoot(dir)
+		charge_time = 0
+
+	print(charge_time)
+
 	$Gun.position = dir * 100
+
+func shoot(dir):
+	linear_velocity -= dir * 100 * charge_time
+
