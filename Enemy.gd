785add69997c0d54042f743ccfb95bd4b1fd3ccf
jakubtomsu <66876057+jakubtomsu@users.noreply.github.com>
2022-10-15 14:33:03 +0200

    Update

---
+++ Enemy.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+export var health: float = 4
+export var damage: float = 1
+export var speed: float = 1
+export var health_drop_chance: float = 0.33
+
+
+func _process(delta):
+	pass
+
+
+func kill():
+	pass
--- Player.gd
+++ Player.gd
@@ -1,55 +1,105 @@
 extends KinematicBody2D
 
 onready var level = get_parent()
-var speed = Vector2(500, 1000)
-var gravity = 2500
-var friction = 0.2
-var air_friction = 0.04
-var acceleration = 0.1
+onready var body = $Body
+onready var gun = $Gun
+onready var raycast = $RayCast2D
+
+onready var shoot_hit_effect = preload("res://PlayerShootHitEffect.tscn")
+
+const SHOOT_JUMP_SPEED = 450
+const MAX_AMMO = 12
+
+var speed = Vector2(200, 250)
+var gravity = 900
+var friction = 0.5
+var air_friction = 0.05
+var air_friction_y = 0.015
+var acceleration = 0.22
 var velocity = Vector2.ZERO
 var ammo = MAX_AMMO
-const MAX_AMMO = 3
+var is_facing_right: bool = true
+
+var max_health: int = 4
+var health: int = max_health
 
 
-func _physics_process(delta: float) -> void:
-	if is_on_floor():
-		ammo = MAX_AMMO
-	
-	var dir = get_direction()
-	var is_jump_interupted = Input.is_action_just_released("jump") and velocity.y < 0.0
-	velocity = calculate_velocity(velocity, dir, is_jump_interupted, speed, delta)
+func _physics_process(delta: float):
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-
-func get_direction():
-	var result: Vector2
-	result.x = Input.get_action_strength("player_right") - Input.get_action_strength("player_left")
-		
-	if ammo > 0 && Input.is_action_just_pressed("player_shoot"):
-		result += Vector2.UP
-		ammo -= 1
-		level.switch_platforms()
-
-	return result
+func _process(delta):
+	if Input.is_key_pressed(KEY_R):
+		ammo = MAX_AMMO
 	
-
-func calculate_velocity(curVelocity, dir, is_jump_interupted, speed, delta):
-	var out = Vector2.ZERO
-
-	if dir.x != 0:
-		out.x = lerp(curVelocity.x, dir.x * speed.x, acceleration)
+	var move_dir: = Input.get_action_strength("player_right") - \
+		Input.get_action_strength("player_left")
+	
+	if move_dir != 0:
+		velocity.x = lerp(velocity.x, move_dir * speed.x, acceleration)
 	else:
-		out.x = lerp(curVelocity.x, 0, friction if is_on_floor() else air_friction)
-
-	if(dir.y == -1.0): # Jumping
-		out.y = dir.y * speed.y
+		velocity.x = lerp(velocity.x, 0, friction if is_on_floor() else air_friction)
+	
+	velocity.y = lerp(velocity.y, 0, air_friction_y)
+		
+	# Jumping
+	if Input.is_action_just_pressed("player_jump") && is_on_floor():
+		velocity = Vector2.UP * speed.y
 	else: # Falling
-		out.y = curVelocity.y + (gravity * delta)
+		velocity += Vector2.DOWN * gravity * delta
+	
+	var aim_dir = get_aim_dir()
+		
+	if Input.is_action_just_pressed("player_shoot"):
+		shoot(aim_dir)
+	
+	if Input.is_action_just_pressed("player_jump") && !is_on_floor():
+		shoot(Vector2.DOWN)
 
-	if(is_jump_interupted): # jump key release
-		out.y = 0.0 + (gravity * delta)
+	gun.position = aim_dir * 30
+	gun.rotation = aim_dir.angle()
 
-	return out
+	if Input.is_action_just_pressed("player_left"):
+		is_facing_right = false
+	if Input.is_action_just_pressed("player_right"):
+		is_facing_right = true
+	body.scale.x = abs(body.scale.x) * (1 if is_facing_right else -1)
+
+
+	if health <= 0:
+		kill()
+
+
+func get_aim_dir() -> Vector2:
+	var face_dir = (Vector2.RIGHT if is_facing_right else Vector2.LEFT)
+	return face_dir
+
+
+func take_damage(damage: int):
+	health -= damage
+	print("[Player] take_damage: ", damage, " health: ", health)
+
 
 func kill():
 	level.restart()
+
+
+func shoot(dir: Vector2):
+	print("[Player] shoot dir: ", dir)
+	if ammo > 0:
+		var force: float = SHOOT_JUMP_SPEED * 0.2 if is_on_floor() else SHOOT_JUMP_SPEED
+		velocity = -dir * speed
+		ammo -= 1
+		
+		raycast.enabled = true
+		raycast.set_cast_to(dir * 1000)
+		raycast.force_raycast_update()
+		if raycast.is_colliding():
+			print(raycast.get_collision_point())
+			var effect = shoot_hit_effect.instance()
+			effect.position = raycast.get_collision_point()
+			get_parent().add_child(effect)
+			var c = raycast.get_collider()
+			if c.has_method("on_shot"):
+				c.on_shot()
+		raycast.enabled = false
+		
